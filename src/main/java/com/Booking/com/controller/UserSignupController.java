63fc04613e72124aad897804aff353b32150e240
Rakesh Kumar <webmkumar@gmail.com>
2025-05-21 01:25:27 +0530

    Login featres success

--- src/main/java/com/Booking/com/controller/UserSignupController.java
+++ src/main/java/com/Booking/com/controller/UserSignupController.java
@@ -1,6 +1,8 @@
 package com.Booking.com.controller;
 
 
+import com.Booking.com.dto.LoginDto;
+import com.Booking.com.dto.TokenResponse;
 import com.Booking.com.dto.UserSignupDto;
 import com.Booking.com.entity.UserSignup;
 import com.Booking.com.service.UserSignupService;
@@ -23,7 +25,7 @@ public class UserSignupController {
 
 
     // Endpoint for user signup
-    // Base URL: localhost:8083/api/v1/signup
+    // Base URL: http://localhost:8083/api/v1/signup
 
     @PostMapping("/signup")
     public ResponseEntity<String> signupUser(@RequestBody UserSignupDto userSignupDto){
@@ -33,5 +35,22 @@ public class UserSignupController {
             return new ResponseEntity("Registration is successful", HttpStatus.CREATED);
         }
         return new ResponseEntity("Something went wrong", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+
+
+
+    // Endpoint for user login
+    // Base URL: http://localhost:8083/api/v1/login
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginDto loginDto) {
+        String token = userSignupService.verifyLogin(loginDto);
+        if (token!= null){
+            TokenResponse tokenResponse = new TokenResponse();
+            tokenResponse.setToken(token);
+            return new ResponseEntity<>(tokenResponse, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Invalid crdentials", HttpStatus.UNAUTHORIZED);
     }
 }
--- src/main/java/com/Booking/com/repository/UserSignupRepository.java
+++ src/main/java/com/Booking/com/repository/UserSignupRepository.java
@@ -3,5 +3,9 @@ package com.Booking.com.repository;
 import com.Booking.com.entity.UserSignup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserSignupRepository extends JpaRepository<UserSignup, Long> {
+
+    Optional<UserSignup> findByUsername(String username);
 }
